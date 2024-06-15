e72bbd4ed3209bbe2df2d33b0b97b7242501e3c9
skaness8290 <skaness8290@student.faytechcc.edu>
2024-06-15 18:29:33 -0400

    Add files via upload

---
+++ P1LAB1_SkanesShanice.py
@@ -0,0 +1,20 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Shanice Skanes
+>>> #06/12/2024
+>>> #P1Lab1
+>>> #Using python to program basic input and output functions
+>>> first_name = input()
+last_name = input()
+>>> print('Enter your first name', first_name)
+Enter your first name last_name = input()
+>>> print('Enter your las name:', last_name)
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    print('Enter your las name:', last_name)
+NameError: name 'last_name' is not defined. Did you mean: 'first_name'?
+>>> print('Hello', first_name, last_name, 'Welcome to CTI 110')
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    print('Hello', first_name, last_name, 'Welcome to CTI 110')
+NameError: name 'last_name' is not defined. Did you mean: 'first_name'?
