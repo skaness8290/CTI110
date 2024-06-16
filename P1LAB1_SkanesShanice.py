8ce46dca1ff7a20f74d43946fb4e7aa9061184c9
skaness8290 <skaness8290@student.faytechcc.edu>
2024-06-15 21:55:18 -0400

    Add files via upload
    Attempt 2

--- P1LAB1_SkanesShanice.py
+++ P1LAB1_SkanesShanice.py
@@ -1,20 +1,26 @@
 Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> #Shanice Skanes
->>> #06/12/2024
->>> #P1Lab1
->>> #Using python to program basic input and output functions
->>> first_name = input()
-last_name = input()
->>> print('Enter your first name', first_name)
-Enter your first name last_name = input()
->>> print('Enter your last name:', last_name)
+>>> FirstName = input("firstname")
+firstname
+>>> Lastname = input("lastname")
+lastname
+>>> print("Enter you first name:", FirstName)
+Enter you first name: 
+>>> print("Enter your last name:", Lastname)
+Enter your last name: 
+>>> print("Hello!", Firstname, Lastname, "Welcome to CTI110!!")
 Traceback (most recent call last):
-  File "<pyshell#6>", line 1, in <module>
-    print('Enter your las name:', last_name)
-NameError: name 'last_name' is not defined. Did you mean: 'first_name'?
->>> print('Hello', first_name, last_name, 'Welcome to CTI 110')
+  File "<pyshell#4>", line 1, in <module>
+    print("Hello!", Firstname, Lastname, "Welcome to CTI110!!")
+NameError: name 'Firstname' is not defined. Did you mean: 'FirstName'?
+>>> print("Hello", Firstname, "Welcome to CTI110!")
 Traceback (most recent call last):
-  File "<pyshell#7>", line 1, in <module>
-    print('Hello', first_name, last_name, 'Welcome to CTI 110')
-NameError: name 'last_name' is not defined. Did you mean: 'first_name'?
+  File "<pyshell#5>", line 1, in <module>
+    print("Hello", Firstname, "Welcome to CTI110!")
+NameError: name 'Firstname' is not defined. Did you mean: 'FirstName'?
+>>> print("Hello", FirstName, Lastname, "Welcome to CTI110!")
+Hello   Welcome to CTI110!
+>>> #ShaniceSkanes
+>>> #06/15/24
+>>> #P1
+>>> #P1Lab2
